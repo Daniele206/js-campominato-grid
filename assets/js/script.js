82549dd4062d9dc747e67365f4fc89c252ad3994
Daniele206 <danieledeieso2004@gmail.com>
2024-02-29 17:12:28 +0100

    [add] grid complete game

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,40 @@ const numCell = 100;
 // Input
 
 // Button
-const playGameBtn = document.getElementById('paly-game-btn');
+const playGameBtn = document.getElementById('play-game-btn');
+const resetGameBtn = document.getElementById('reset-game-btn');
 
 // Output
 
 // Functions
+function rNone(element){
+  element.classList.remove('d-none');
+  element.classList.add('d-flex');
+}
+
 function addCell(num){
-  myGrid.innerHTML +=`
-  <div class="my-cell"><p class="d-none">${num}</p></div>
-  `
+  const cl = document.createElement('div');
+  cl.classList.add('my-cell');
+  cl.addEventListener('click', function (){
+    this.classList.toggle('active');
+    this.innerHTML = (this.classList.contains('active')) ? this.innerHTML = num  : this.innerHTML = '';
+  });
+
+  resetGameBtn.addEventListener('click', function(){
+    cl.classList.remove('active');
+    cl.innerHTML = '';
+  });
+  
+  return cl;
 }
 
 //------------------------------------------------------------------
 
+playGameBtn.addEventListener('click', function(){
+  rNone(myGrid);
+});
+
 for(let i=1; i <= numCell; i++){
-  addCell(i);
+  const cell = addCell(i)
+  myGrid.append(cell)
 }

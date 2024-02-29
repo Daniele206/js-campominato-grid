de5ca078c9ce147b38e6043e1fe70d9c1d9cb24d
Daniele206 <danieledeieso2004@gmail.com>
2024-02-29 16:24:02 +0100

    [add] grid js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,25 @@
 // Element
 const myGrid = document.querySelector('.my-grid');
+const numCell = 100;
 
 // Element-none
 
 // Input
 
 // Button
-const playGameBtn = document.getElementById('paly-game-btn')
+const playGameBtn = document.getElementById('paly-game-btn');
 
 // Output
 
 // Functions
+function addCell(num){
+  myGrid.innerHTML +=`
+  <div class="my-cell"><p class="d-none">${num}</p></div>
+  `
+}
 
 //------------------------------------------------------------------
+
+for(let i=1; i <= numCell; i++){
+  addCell(i);
+}

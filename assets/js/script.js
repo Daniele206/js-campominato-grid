843308c2846828bb12e56924296162aa9b9d73eb
Daniele206 <danieledeieso2004@gmail.com>
2024-02-29 18:47:44 +0100

    [add] bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
 // Element
 const myGrid = document.querySelector('.my-grid');
-const numCell = 100;
+const easy = document.getElementById('easy');
+const normal = document.getElementById('normal');
+const hard = document.getElementById('hard');
 
 // Element-none
+let numCell;
+let difficultySelected;
 
 // Input
+const selectDifficulty = document.querySelector('.select-difficulty')
 
 // Button
 const playGameBtn = document.getElementById('play-game-btn');
@@ -18,9 +23,18 @@ function rNone(element){
   element.classList.add('d-flex');
 }
 
-function addCell(num){
+function addCell(num, difficulty){
   const cl = document.createElement('div');
   cl.classList.add('my-cell');
+
+  if(difficulty.value === 'easy'){
+    cl.classList.add('whidth-easy');
+  }else if(difficulty.value === 'normal'){
+    cl.classList.add('whidth-normal');
+  }else if(difficulty.value === 'hard'){
+    cl.classList.add('whidth-hard') ;
+  };
+
   cl.addEventListener('click', function (){
     console.log(num);
     this.classList.toggle('active');
@@ -33,15 +47,32 @@ function addCell(num){
   });
 
   return cl;
-}
+};
 
 //------------------------------------------------------------------
+easy.addEventListener('click', function(){
+  location.reload();
+})
+normal.addEventListener('click', function(){
+  location.reload();
+})
+hard.addEventListener('click', function(){
+  location.reload();
+})
 
 playGameBtn.addEventListener('click', function(){
   rNone(myGrid);
 });
 
+if(selectDifficulty.value === 'easy'){
+  numCell = 100;
+}else if(selectDifficulty.value === 'normal'){
+  numCell = 81;
+}else if(selectDifficulty.value === 'hard'){
+  numCell =49;
+};
+
 for(let i=1; i <= numCell; i++){
-  const cell = addCell(i);
+  const cell = addCell(i, selectDifficulty);
   myGrid.append(cell);
 }
